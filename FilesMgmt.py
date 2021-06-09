ab02718f40a9642b90fd8a4456a933a785fd70c4
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-06-08 23:17:08 -0500

    add a new student was implemented successfully

--- FilesMgmt.py
+++ FilesMgmt.py
@@ -8,17 +8,53 @@ OPTIONS = {
     "add":'3',
     "remove":'4',
     "clean":'5',
-    "exit":'6',
+    "save":'6',
+    "exit":'7',
 }
 
+# ------------------------ DF ------------------------
+def df_create():
+    results = {
+        "created" : False
+    }
+    try:
+        fields = [
+            'DNI', 
+            'NAME', 
+            'GRADES', 
+            'COMMENTS', 
+        ]
+
+        df = pd.DataFrame({}, columns=fields, index={})
+        
+        results["df"] = df
+        results["created"] = True
+        return results
+    except:
+        print(f"Some error occurred while creating the dataframe")
+        return results
+
+def df_isEmpty(df:DataFrame):
+    return df.empty
+
+def df_showData(df:DataFrame):
+    if (df_isEmpty(df)):
+        print("\nIt is empty. Please add at least 1 student\n")
+        return
+    print(df)
+
+def df_addNew(df:DataFrame, args:list)->DataFrame:
+    df = df.append(pd.Series(args, index=df.columns), ignore_index=True)
+    return df
+# ---------------------- END DF ----------------------
 # ----------------------- CSV  -----------------------
 def CSV_checkExistence(filename:str)->dict:
     results = {
         "found" : False
     }
     try:
-        df = pd.read_csv(filename, skiprows=1, names=['dni', 'name', 'grades', 'comments'])
-        results["dataFrame"] = df
+        df = pd.read_csv(filename, skiprows=1, names=["DNI", "NAME", "GRADES", "COMMENTS"])
+        results["df"] = df
         results["found"] = True
         return results
     except:
@@ -29,16 +65,11 @@ def CSV_createFile(filename:str)->dict:
         "created" : False
     }
     try:
-        students_data = {
-            'dni': [],
-            'name': [],
-            'grades': [],
-            'comments': [],
-        }
-
-        df = pd.DataFrame(students_data)
+        res = df_create()
+        df = res["df"]
+        
         df.to_csv(filename)
-        results["dataFrame"] = df
+        results["df"] = df
         results["created"] = True
         return results
     except:
@@ -47,24 +78,15 @@ def CSV_createFile(filename:str)->dict:
 
 def CSV_open(filename:str)->DataFrame:
     file = CSV_checkExistence(filename)
+
     if file["found"] == True:
         print("DB opened successfully")
     else:
         file = CSV_createFile(filename)
         print("DB created")
     
-    return file['dataFrame']
+    return file['df']
 # --------------------- END CSV  ---------------------
-# ------------------------ DF ------------------------
-def df_isEmpty(df:DataFrame):
-    return df.empty
-
-def df_showData(df:DataFrame):
-    if (df_isEmpty(df)):
-        print("\nIt is empty. Please add at least 1 student\n")
-        return
-    print(df)
-# ---------------------- END DF ----------------------
 # ----------------------- VIEW -----------------------
 def printStartup():
     print(f'+-----------------------------+\nPress 1 to open a CSV file\n+-----------------------------+')
@@ -74,12 +96,28 @@ def printMenu():
     print("Select an option:")
     print("1. Check all students")
     print("2. Modify a student info") # TODO
-    print("3. Add student")
-    print("4. Delete student")
+    print("3. Add student (No database)")
+    print("4. Delete student (No database)")
     print("5. Clean all students info")
-    print("6. Exit")
+    print("6. Save changes")
+    print("7. Exit")
     print("+-----------------------------+")
 # --------------------- END VIEW ---------------------
+# ---------------------- STUDENT ---------------------
+def student_addNew(df:DataFrame):
+    dni = input('Insert DNI: ')
+    name = input('Insert name: ')
+    grade = float(input('Insert final grade: ')) #TODO: Implement a list of notes
+    comments = input('Insert comments about the student: ')
+    student = [
+        dni,
+        name.lower(),
+        grade,
+        comments.lower(),
+    ]
+
+    return df_addNew(df, student)
+# -------------------- END STUDENT -------------------
 
 def infiniteLoop(df:DataFrame):
 
@@ -94,12 +132,16 @@ def infiniteLoop(df:DataFrame):
             
             continue
         elif user == OPTIONS['add']:
-            
+            df = student_addNew(df)
             continue
         elif user == OPTIONS['remove']:
             
             continue
         elif user == OPTIONS['clean']:
+            
+            continue
+        elif user == OPTIONS['save']:
+            #CSV_save(df)
             
             continue
         elif user == OPTIONS['exit']:
@@ -117,7 +159,7 @@ def run():
         exit()
 
     df = CSV_open('students_db.csv')
-
+    print(df)
     infiniteLoop(df)
 
 run()
