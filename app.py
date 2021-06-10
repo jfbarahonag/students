33f28f7c129820d7488cb340b3203a28692fe0b8
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-06-10 00:30:09 -0500

    student functions were moved to a model folder

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from database import *
 from file import *
+from model import *
 
 from pandas.core.frame import DataFrame
 
@@ -29,40 +30,6 @@ def printMenu():
     print("7. Exit")
     print("+-----------------------------+")
 # --------------------- END VIEW ---------------------
-# ---------------------- STUDENT ---------------------
-def student_update(df:DataFrame):
-    df_showData(df)
-
-    idx = int(input('Please insert the index of the student: '))
-    field = input('Please insert the field you want change: ')
-    val = input(f'Please insert the new value to the {field}: ')
-
-    return df_update(df, (idx, field, val))
-
-def student_addNew(df:DataFrame):
-    dni = input('Insert DNI: ')
-    name = input('Insert name: ')
-    grade = float(input('Insert final grade: ')) #TODO: Implement a list of notes
-    comments = input('Insert comments about the student: ')
-    student = [
-        dni,
-        name.capitalize(),
-        grade,
-        comments.lower(),
-    ]
-
-    return df_addNew(df, student)
-
-def student_remove(df:DataFrame):
-    df_showData(df)
-
-    idx = int(input('Please insert the index of the student: '))
-
-    return df_remove(df, idx)
-
-def student_cleanAll():
-    return df_reset()
-# -------------------- END STUDENT -------------------
 
 def infiniteLoop(df:DataFrame):
 
